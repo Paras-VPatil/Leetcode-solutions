1d76d6dfeb0f4fa05903bda311cd1899f467e4dd
Paras Patil <140316241+Paras-VPatil@users.noreply.github.com>
2026-08-06 22:32:43 +0530

    Time: 1 ms (97.67%) | Memory: 47.4 MB (18.07%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2*nums.length];
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+        }
+        for(int j=n;j<2*n;j++){
+            ans[j]=nums[j-n];
+        }
+        return ans;
+
+    }
+}
