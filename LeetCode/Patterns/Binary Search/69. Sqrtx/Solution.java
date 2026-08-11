9e4b1ca2e9f74c47026f45321f295dc0b70ede32
Paras Patil <140316241+Paras-VPatil@users.noreply.github.com>
2026-08-11 10:00:49 +0530

    Add Java Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.java
+++ LeetCode/Patterns/Binary Search/69. Sqrtx/Solution.java
@@ -1,20 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
+        if (x < 2) return x; 
         int diff = Integer.MAX_VALUE;
-        int sqrt=0;
-        for(int i=1;i<=x/2;i++){
-            if (x/i==i){
-                sqrt=i;
-                break;
-            }
-            else{
-                int q=x/i;
-                if(q>i && diff>q-i){
-                    diff=q-i;
-                    sqrt=i;
+        int sqrt = 0;
+
+        for (int i = 1; i <= x / 2; i++) {
+            int q = x / i;
+            if (q == i) {
+                return i;
+            } else if (q > i) {
+                if (diff > q - i) {
+                    diff = q - i;
+                    sqrt = i;
                 }
+            } else {
+                break; 
             }
-        }return sqrt;
+        }
+        return sqrt;
 
     }
 }
