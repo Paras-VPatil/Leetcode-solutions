2fb60953daf3c057d64bf56844c8416912d1c4a9
Paras Patil <140316241+Paras-VPatil@users.noreply.github.com>
2026-08-13 15:28:57 +0530

    Time: 6 ms (7.7%), Space: 46.7 MB (37.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,6 +1,5 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<String,Integer> roman =new HashMap<>();
         Map<String, Integer> roman1 = Map.of("I", 1, "V", 5,"X",10,"L",50,"C",100,"D",500,"M",1000);
         int cardinal=0;
         for(int i=0;i<s.length();i++){
